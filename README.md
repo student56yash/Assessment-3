1bb2908596e60686b67625bab0a4f4faffb66307
student56yash <you@example.com>
2020-05-05 15:11:25 +1000

    Making Readme file and assessment 3 part 1 attempt 01

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Assessment-3
-SLE712 Bioinformatics assessment details
+SLE712 Bioinformatics and Molecular Biology Techniques
+
+## Project title
+SLE712-Assessment 3- part 1: Importing files,data wrangling, mathematical operations,plots and saving code on GitHub
+
+## Motivation
+Develop problem solving skills, R coding, and improve the team working ability with the use of Rstudio and GitHub
+
+## Content
+* Readme file in markdown
+* Data files: "gene_expression.tsv" and "growth_data.csv"
+* Assessment 3 part 1 Rscript/Rmd
+
+## Instalation
+Rstudio Version 1.2.1335 and R version 3.6.3
+
+## Contribute
+
+## Credits
+Yasodha Suharshi Jayawardena & 
+
+## License
+
+MIT © [Yashoda Suharshi]()
+
+
+
+
+
---
+++ attempt1part1.R
@@ -0,0 +1,43 @@
+# Downlod the data file of "gene_expression.tsv"
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/gene_expression.tsv",destfile="gene_expression.tsv")
+
+# Read into R 
+x<-read.table("gene_expression.tsv")
+# Lets check data type and if the data has been imported properly
+typeof(x)
+head(x)
+str(x)
+
+# Making the gene accession numbers the row name
+x <- read.table("gene_expression.tsv", header = TRUE)
+head(x)
+str(x)
+x <- read.table("gene_expression.tsv", header = TRUE , stringsAsFactors = FALSE )
+head(x)
+str(x)
+x <- read.table("gene_expression.tsv", header = TRUE , stringsAsFactors = FALSE , row.names = 1)
+head(x)
+str(x)
+
+x <- read.csv("gene_expression.tsv", sep="\t" ,stringsAsFactors = FALSE, row.names = 1 )
+head(x)
+str(x)
+
+#Make a new column which is the mean of other column
+x$A <- colMeans(x)
+x
+colnames(x)[3]<-"Mean"
+head(x)
+str(x)
+
+#need to fix ; list 10 genes with the highest mean expression
+sort(A,decreasing=TRUE,n=10)
+
+head(sort(df(a), decreasing=TRUE), n=10)
+
+a <-colMeans(x)
+sort(a,decreasing=TRUE,10)
+
+
+
+
